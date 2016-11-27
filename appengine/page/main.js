40dd4fbda69cd474af3d8d1e59899d8f8dbf721f
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2016-11-27 13:45:05 +0100

    Made the appengine page look a bit nicer

--- appengine/page/main.js
+++ appengine/page/main.js
@@ -4,10 +4,14 @@ function main()
     // python code in the appengine project retrieves it and forwards
     // the result
     var url = "/javascriptcode.js";
+
+    if (document.location.hostname == "localhost") // for debugging purposes
+        url = "https://raw.githubusercontent.com/j0r1/GitHubPrintMarkDown/master/javascriptcode.js";
     
     var responseFunction = function(data)
     {
         console.log(data);
+        $("#bookmarkletcode").html(data);
 
         var javascriptPrefix = "javascript:";
         if (data.substr(0, javascriptPrefix.length) == javascriptPrefix)
